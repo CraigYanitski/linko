ffa4e6ff64f91cc9a6d6bb90cf9b3acede3b9534
Craig Yanitski <yanitski@ph1.uni-koeln.de>
2026-03-28 21:26:09 -0600

    setup two loggers for the program

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
+	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -24,28 +25,47 @@ func main() {
 	os.Exit(status)
 }
 
+func requestLogger(logger *log.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			next.ServeHTTP(w, r)
+			logger.Printf("Served request: %s %s\n", r.Method, r.URL.Path)
+		})
+	}
+}
+
 func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir string) int {
-	st, err := store.New(dataDir)
+	logFile, err := os.Create("linko.access.log")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to create store: %v\n", err)
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	var accessLogger = log.New(logFile, "INFO: ", log.LstdFlags)
+	var stdLogger = log.New(os.Stderr, "DEBUG: ", log.LstdFlags)
+
+	st, err := store.New(dataDir, stdLogger)
+	if err != nil {
+		log.Printf("failed to create store: %v\n", err)
 		return 1
 	}
-	s := newServer(*st, httpPort, cancel)
+	s := newServer(*st, httpPort, cancel, accessLogger)
 	var serverErr error
 	go func() {
 		serverErr = s.start()
 	}()
 
 	<-ctx.Done()
+	stdLogger.Println("Linko is shutting down")
 	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	if err := s.shutdown(shutdownCtx); err != nil {
-		fmt.Fprintf(os.Stderr, "failed to shutdown server: %v\n", err)
+		log.Printf("failed to shutdown server: %v\n", err)
 		return 1
 	}
 	if serverErr != nil {
-		fmt.Fprintf(os.Stderr, "server error: %v\n", serverErr)
+		log.Printf("server error: %v\n", serverErr)
 		return 1
 	}
 	return 0
